406312b5d9755d876913553e2c8711d0f849a1ce
Davonte <dbertrandmurrell@trentu.ca>
2021-02-08 23:16:05 -0500

    Clean Calc program with citations

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-
-
-
         public static void main(String args[]) {
 
             //input to print Fibonacci series upto how many numbers
             System.out.println("Welcome To The Fibonacci Calculator!");
             System.out.println("Enter The Number Of Sequences You Want To Calculate: ");
-
+            //Take user input as n
             int n = new Scanner(System.in).nextInt();
 
-            System.out.println("Fibonacci series upto " + n +" numbers : ");
-            //printing Fibonacci series up to number
+            System.out.println("These are the Fibonacci Sequences Of " + n + " Terms: ");
+
             for(int i=1; i<=n; i++){
-                System.out.print(fiboSeriesIte(i) +" ");
+                System.out.print(fiboSeriesRec(i) +" , ");
             }
 
 
@@ -24,33 +21,30 @@ public class Main {
 
 
                                 // Recursion Function
-        public static int fiboSeriesRec(int n){
+// [Citation: https://www.java67.com/2016/05/fibonacci-series-in-java-using-recursion.html]
+        public static int fiboSeriesRec(int n) {
             if(n == 1 || n == 2){
                 return 1;
             }
-
             return fiboSeriesRec(n-1) + fiboSeriesRec(n -2);
         }
-dsd
 
-        //Iteration Function
+
+                                //Iteration Function
+// [Citation: https://www.programiz.com/java-programming/examples/fibonacci-series ]
         public static int fiboSeriesIte(int n){
             if(n == 1 || n == 2){
                 return 1;
             }
-
             int t1 = 0;
             int t2 = 1;
             int sum=1;
 
             for (int i = 1; i <= n; ++i)
             {
-                System.out.print(t1 + " , ");
-
                 sum = t1 + t2;
                 t1 = t2;
                 t2 = sum;
-
             }
             return sum;
 
